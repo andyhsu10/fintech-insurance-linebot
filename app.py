1950f7109ce83d415c2fcb14b123e976e7ed7f68
An-Ting Hsu <andyhsu10@gmail.com>
2019-06-02 01:30:32 +0800

    test date button

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        if event.postback:
-            message = TextSendMessage(text=event.postback)
+        if event.postback.params.date:
+            message = TextSendMessage(text=event.postback.params.date)
         else:
             message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
