3e331d18141894f88e7783bdc6b91f6d69d8b4fc
An-Ting Hsu <andyhsu10@gmail.com>
2019-06-02 20:35:43 +0800

    global var fix

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global insurance
     if event.message.text == '開始使用':
         insurance = InsuranceBot()
         line_bot_api.reply_message(event.reply_token, insurance.msg)
